83908dd809527c1c06246909e8c579d31198b5d8
sakklavut <sakklavut23395@gmail.com>
2023-05-02 17:52:24 +0700

    แก้ไขดีไซน หน้า index ใหม่

--- banner.php
+++ banner.php
@@ -3,19 +3,21 @@
           <div class="col-xl-3 col-sm-6 col-md-6">
                <div class="box1_banner">
                     <div class="row">
-                         <div class="col-xl-12">
-                              <img class="icon_main_banner_1"  src="icon/Complain.svg" alt="">
+                         <div class="col-xl-12 text-center mt-5">
+                              <img src="icon/Complain.svg" alt="">
                          </div>
                     </div>
                     <div class="row">
                          <div class="col-xl-12">
                               <div class="sup_box1_banner">
                                    <div class="row">
-                                        <div class="col-xl-3  col-3">
-                                             <img src="icon/icon_1.svg" alt="">
-                                        </div>
-                                        <div class="col-xl-9   col-9 p-1">
-                                             <a href="agreement_complaint.php"> <h4 class="text-white text_compla">ร้องเรียนร้องทุกข์บัตรสนเท่ห์</h4> </a>
+                                        <div class="d-flex">
+                                             <div class="box_icon_banner">
+                                                  <img class="img-fluid" src="icon/icon_1.svg" alt="">
+                                             </div>
+                                             <div class="box_text_banner">
+                                                  <a href="agreement_complaint.php"> <h4 class="text-white text_compla">ร้องเรียนร้องทุกข์บัตรสนเท่ห์</h4></a>
+                                             </div>
                                         </div>
                                    </div>
                               </div>
@@ -26,19 +28,21 @@
           <div class="col-xl-3 col-sm-6 col-md-6">
                <div class="box2_banner">
                     <div class="row">
-                         <div class="col-xl-12">
-                              <img class="icon_main_banner_2"  src="icon/Message.svg" alt="">
+                         <div class="col-xl-12 text-center mt-5">
+                              <img src="icon/Message.svg" alt="">
                          </div>
                     </div>
                     <div class="row">
                          <div class="col-xl-12">
                               <div class="sup_box2_banner">
                                    <div class="row">
-                                        <div class="col-xl-3  col-3">
-                                             <img src="icon/icon_2.svg" alt="">
-                                        </div>
-                                        <div class="col-xl-9  col-9">
-                                            <a href="clue.php"> <h4 class="text-white mt-3 text_compla">แจ้งเบาะแส</h4></a>
+                                        <div class="d-flex">
+                                             <div class="box_icon_banner">
+                                                  <img src="icon/icon_2.svg" alt="">
+                                             </div>
+                                             <div class="box_text_banner_2">
+                                                  <a href="clue.php"> <h4 class="text-white text_compla">แจ้งเบาะแส</h4></a>
+                                             </div>
                                         </div>
                                    </div>
                               </div>
@@ -50,19 +54,21 @@
                <div class="box3_banner">
                     <div class="box3_banner">
                          <div class="row">
-                              <div class="col-xl-12">
-                                   <img class="icon_main_banner_3"  src="icon/Chat.svg" alt="">
+                              <div class="col-xl-12 text-center mt-5">
+                                   <img src="icon/Chat.svg" alt="">
                               </div>
                          </div>
                          <div class="row">
                               <div class="col-xl-12">
                                    <div class="sup_box3_banner">
                                         <div class="row">
-                                             <div class="col-xl-3  col-3">
-                                                  <img src="icon/icon_3.svg" alt="">
-                                             </div>
-                                             <div class="col-xl-9  col-9">
-                                                <a href="feedback.php">  <h4 class="text-white mt-3 text_compla">ข้อเสนอแนะ</h4></a>
+                                             <div class="d-flex">
+                                                  <div class="box_icon_banner">
+                                                       <img src="icon/icon_3.svg" alt="">
+                                                  </div>
+                                                  <div class="box_text_banner_3">
+                                                       <a href="clue.php"> <h4 class="text-white text_compla">ข้อเสนอแนะ</h4></a>
+                                                  </div>
                                              </div>
                                         </div>
                                    </div>
@@ -74,19 +80,21 @@
           <div class="col-xl-3 col-sm-6 col-md-6">
                <div class="box4_banner">
                     <div class="row">
-                         <div class="col-xl-12">
-                              <img class="icon_main_banner_4"  src="icon/image 2.svg" alt="">
+                         <div class="col-xl-12 text-center mt-5">
+                              <img src="icon/image 2.svg" alt="">
                          </div>
                     </div>
                     <div class="row">
                          <div class="col-xl-12">
                               <div class="sup_box4_banner">
                                    <div class="row">
-                                        <div class="col-xl-3 col-3">
-                                              <img src="icon/icon_4.svg" alt="">
-                                         </div>
-                                         <div class="col-xl-9 col-9">
-                                            <a href="follow_complaints.php"> <h4 class="text-white mt-3 text_compla">ติดตามเรื่องร้องเรียน</h4></a>
+                                        <div class="d-flex">
+                                             <div class="box_icon_banner">
+                                                  <img src="icon/icon_4.svg" alt="">
+                                             </div>
+                                             <div class="box_text_banner_4">
+                                                  <a href="follow_complaints.php"> <h4 class="text-white text_compla">ติดตามเรื่องร้องเรียน</h4></a>
+                                             </div>
                                         </div>
                                    </div>
                               </div>
